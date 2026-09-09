a05efa6343367c798f652e3c08cd7860ac33af79
Paradoxzyy <228410201+Paradoxzyy@users.noreply.github.com>
2026-09-10 10:40:11 +1200

    Convert to arrow-js

---
+++ js/main.js
@@ -0,0 +1,515 @@
+import { reactive, html, svg, component, watch } from "https://esm.sh/@arrow-js/core@1.0.6"
+
+// Hacks
+HTMLCollection.prototype.forEach = Array.prototype.forEach
+
+const state = reactive({})
+const global = {}
+
+//--------------------------------------------------------------------------------
+//---------------------------------------- Init ----------------------------------
+//--------------------------------------------------------------------------------
+const init = async () => {
+  await loadData()
+  loadURL()
+  html`${Root()}`(document.body)
+  watch(() => state.skillAllocation, handleDrawLines)
+  handleDrawLines()
+}
+
+//--------------------------------------------------------------------------------
+//---------------------------------------- Data ----------------------------------
+//--------------------------------------------------------------------------------
+const loadData = async () => {
+  const files = [ "skills", "classes", "meta" ]
+  const folder = location.origin + location.pathname
+  const promises = await Promise.allSettled(files.map(file => fetch(`${folder}${file}.json`).then(res => res.json())))
+
+  promises.forEach((promise, i) => {
+    const file = files[i]
+
+    if (promise.status == "fulfilled") {
+      global[file] = promise.value
+      Object.freeze(global[file])
+    }
+    else
+      console.warn(`Failed loading: ${file}.json`)
+  })
+
+  state.currentClass = 0
+  state.currentLevel = 1
+  state.currentRetirement = 0
+
+  changeClass()
+}
+
+//--------------------------------------------------------------------------------
+//---------------------------------------- Functions -----------------------------
+//--------------------------------------------------------------------------------
+const changeClass = () => {
+  state.skillAllocation = global.classes[state.currentClass].skills.reduce((acc, curr) => (acc[curr] = 0, acc), {})
+
+  changeSp()
+}
+
+//----------------------------------------
+const changeSp = () => {
+  state.totalSp = global.meta.initialSp + (global.meta.spPerLevel * state.currentLevel) + state.currentRetirement
+  state.freeSp = Object.values(state.skillAllocation).reduce((acc, curr) => acc -= curr, state.totalSp)
+}
+
+//----------------------------------------
+const increaseSkill = (skill, points = 1) => {
+  if (global.skills[skill].upstream) {
+    Object.entries(global.skills[skill].upstream).forEach(([ k, v ]) => {
+      if (state.skillAllocation[k] < v)
+        increaseSkill(k, v - state.skillAllocation[k])
+    })
+  }
+
+  if (state.skillAllocation[skill] < global.skills[skill].maxLevel) {
+    state.skillAllocation[skill] += points
+    state.freeSp -= points
+  }
+
+  saveURL()
+}
+
+//----------------------------------------
+const decreaseSkill = (skill, points = 1) => {
+  if (state.skillAllocation[skill] >= points) {
+    state.skillAllocation[skill] -= points
+    state.freeSp += points
+  }
+
+  if (global.skills[skill].downstream) {
+    Object.entries(global.skills[skill].downstream).forEach(([ k, v ]) => {
+      if (state.skillAllocation[k] > 0 && state.skillAllocation[skill] < global.skills[k].upstream[skill])
+        decreaseSkill(k, state.skillAllocation[k])
+    })
+  }
+
+  saveURL()
+}
+
+//--------------------------------------------------------------------------------
+//---------------------------------------- Components ----------------------------
+//--------------------------------------------------------------------------------
+const Root = component(() => {
+  return html`
+    <div class="header">
+      <div class="logo">
+        <img src="images/Etrian_Odyssey_logo.png" width="180px" height="100px" alt="Header Logo"></img>
+      </div>
+
+      ${Controls()}
+    </div>
+
+    <div class="main">
+      <div class="skill-grid">
+        ${SkillGrid()}
+      </div>
+
+      <div class="svg-grid">
+        ${SvgGrid()}
+      </div>
+    </div>`
+})
+
+//----------------------------------------
+const Controls = component(() => {
+  const handleChangeClass = e => {
+    state.currentClass = +e.target.value
+    changeClass()
+    saveURL()
+  }
+
+  const handleChangeLevel = e => {
+    const value = Math.max(Math.min(+e.target.value, e.target.max), e.target.min)
+
+    if (+e.target.value != value)
+      e.target.value = value
+
+    state.currentLevel = value
+    changeSp()
+    saveURL()
+  }
+
+  const handleChangeRetirement = e => {
+    state.currentRetirement = +e.target.value
+    changeSp()
+    saveURL()
+  }
+
+  return html`
+    <div class="controls-container">
+      <div class="controls">
+        <div>
+          <label>
+            <span>Class</span>
+            ${Select({ name: "class", options: global.classes.map((item, value) => ({ value, text: item.name })), handleChange: handleChangeClass })}
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <span>Level</span>
+            <input type="number" min="1" max="${global.meta.maxLevel}" value="1" name="level" @change="${handleChangeLevel}">
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <span>Retirement</span>
+            ${Select({ name: "retirement", options: global.meta.retirementData, handleChange: handleChangeRetirement })}
+          </label>
+        </div>
+      </div>
+
+      <div>
+        <span>SP: <span class="${() => state.freeSp < 0 ? "overspend" : ""}">${() => state.freeSp}</span> / ${() => state.totalSp}</span>
+      </div>
+    </div>`
+})
+
+//----------------------------------------
+const SkillGrid = component(() => {
+  const data = () => Object.keys(state.skillAllocation)
+    .reduce((acc, curr) => {
+      const item = global.skills[curr]
+
+      // TODO verify all skills exist in skills.json
+      if (!item)
+        return acc
+
+      const index = item.location.x + 6 * item.location.y
+      item.id = curr
+      acc[index] = item
+
+      return acc
+    }, Array(48).fill(null))
+    .map((item, i) => SkillContainer(item).key(`${state.currentClass}-${i}`))
+
+  return html`
+    ${data}`
+})
+
+//----------------------------------------
+const SvgGrid = component(() => {
+  const data = () => Object.keys(state.skillAllocation).map(id => LineContainer({ id }).key(`${state.currentClass}-${id}`))
+
+  return html`
+    ${data}`
+})
+
+//----------------------------------------
+const SkillContainer = component(props => {
+  if (props.id)
+    return html`${Skill(props)}`
+
+  return html`<div></div>`
+})
+
+//----------------------------------------
+const Skill = component(props => {
+  const localState = reactive({
+    showSkillInfo: false
+  })
+
+  const classes = () => createClasses({
+    "skill-box": true,
+    // TODO disabled isn't being readded
+    "disabled": global.skills[props.id].upstream && Object.entries(global.skills[props.id].upstream).some(([ k, v ]) => state.skillAllocation[k] < v)
+  })
+
+  const classesLevel = () => createClasses({
+    "skill-points": true,
+    "active": state.skillAllocation[props.id]
+  })
+
+  const toggleSkillInfo = toggle => localState.showSkillInfo = toggle
+  const level = () => props.maxLevel ? `${state.skillAllocation[props.id]}/${props.maxLevel}` : ""
+
+  const buttons = () => {
+    if (!props.maxLevel)
+      return ""
+
+    return html`
+      <div class="skill-buttons">
+        ${Button({
+          text: "-",
+          disabled: state.skillAllocation[props.id] == 0,
+          handleClick: () => decreaseSkill(props.id)
+        })}
+        ${Button({
+          text: "+",
+          disabled: state.skillAllocation[props.id] == props.maxLevel,
+          handleClick: () => increaseSkill(props.id)
+        })}
+      </div>`
+  }
+
+  return html`
+    <div class="skill">
+      <div class="${classes}" @mouseenter="${() => toggleSkillInfo(true)}" @mouseleave="${() => toggleSkillInfo(false)}">
+        <div class="skill-header">
+          <div class="skill-name">${props.name}</div>
+          <div class="${classesLevel}">${level}</div>
+        </div>
+        ${buttons}
+      </div>
+      ${() => SkillInfo({ ...props, showSkillInfo: localState.showSkillInfo })}
+    </div>`
+})
+
+//----------------------------------------
+const SkillInfo = component(props => {
+  const classes = () => createClasses({
+    "skill-info": true,
+    "display-top": props.location.y > 3,
+    "display-left": props.location.x > 3,
+    "hidden": !props.showSkillInfo
+  })
+
+  const cols = Math.max(props.maxLevel, 5) + 2
+  const col2 = Math.floor(cols / (props.body ? 3 : 2))
+  const col1 = cols - col2 * (props.body ? 2 : 1)
+
+  return html`
+    <div class="${classes}">
+      <table>
+        <tr>
+          <th colspan="${col1}">Name</th>
+          <th colspan="${col2}" class="${props.body ? "" : "hidden"}">Body Part</th>
+          <th colspan="${col2}">Skill Type</th>
+        </tr>
+        <tr>
+          <td colspan="${col1}">${props.name}</td>
+          <td colspan="${col2}" class="${props.body ? "" : "hidden"}">${props.body}</td>
+          <td colspan="${col2}">${props.type}</td>
+        </tr>
+        <tr>
+          <td colspan="${cols}">${props.description}</td>
+        </tr>
+        <tr class="${!props.note ? "hidden" : ""}">
+          <th colspan="${cols}">Note</td>
+        </tr>
+        <tr class="${!props.note ? "hidden" : ""}">
+          <td colspan="${cols}">${props.note}</td>
+        </tr>
+        <tr class="${!props.maxLevel ? "hidden" : ""}">
+          <th colspan="2">Level</th>
+          ${() => Array.from(Array(props.maxLevel).keys()).map(i => html`<th class="${state.skillAllocation[props.id] == i + 1 ? "selected" : ""}">${i + 1}</th>`)}
+        </tr>
+        ${SkillInfoRows(props)}
+      </table>
+    </div>`
+})
+
+//----------------------------------------
+const SkillInfoRows = component(props => {
+  if (!global.skills[props.id].levelData)
+    return html``
+
+  return html`
+    ${() => Object.entries(global.skills[props.id].levelData).map(([ name, data ]) => SkillInfoRow({ name, data, id: props.id, maxLevel: props.maxLevel }))}`
+})
+
+//----------------------------------------
+const SkillInfoRow = component(props => {
+  if (!Array.isArray(props.data)) {
+    return html`
+      <tr>
+        <th colspan="2">${props.name}</th>
+        <td colspan="${props.maxLevel}" class="${state.skillAllocation[props.id] ? "selected" : ""}">${props.data}</td>
+      </tr>`
+  }
+
+  return html`
+    <tr>
+      <th colspan="2">${props.name}</th>
+      ${() => props.data.map((value, i) => html`<td colspan="${props.maxLevel ? 1 : 5}" class="${state.skillAllocation[props.id] == i + 1 ? "selected" : ""}">${value}</td>`)}
+    </tr>`
+})
+
+//--------------------------------------------------------------------------------
+//---------------------------------------- Template Components -------------------
+//--------------------------------------------------------------------------------
+const Select = component(props => {
+  return html`
+    <select name="${props.name}" @change="${props.handleChange}">
+      ${() => props.options.map(item => Option(item).key(item.value))}
+    </select>`
+})
+
+//----------------------------------------
+const Option = component(props => {
+  return html`
+    <option value="${props.value}">${props.text}</option>`
+})
+
+//----------------------------------------
+const Button = component(props => {
+  const classes = () => createClasses({
+    "button": true,
+    "disabled": props.disabled
+  })
+
+  return html`
+    <button class="${classes}" @click="${props.handleClick}">${props.text}</button>`
+})
+
+//----------------------------------------
+const LineContainer = component(props => {
+  if (props.id)
+    return html`${Line(props)}`
+
+  return html`<div></div>`
+})
+
+//----------------------------------------
+const Line = component(props => {
+  // TODO why optional chaining?
+  if (!global.skills[props.id]?.upstream)
+    return html``
+
+  const lines = Object.keys(global.skills[props.id].upstream).map(upstream => {
+    const downskill = global.skills[props.id].location
+    const upskill = global.skills[upstream].location
+
+    const getX = v => 61 + 178 * v
+    const getY = v => 26 + 100 * v
+
+    const upX = getX(upskill.x)
+    const upY = getY(upskill.y)
+    const downX = getX(downskill.x)
+    const downY = getY(downskill.y)
+
+    const x = (downX + upX) / 2
+    const y = (upY + downY) / 2
+
+    const points = `${upX},${upY} ${downX},${downY}`
+    const id = `${upstream}-${props.id}`
+
+    const marker = svg`
+      <marker id="mid" refX="0" refY="3" orient="auto" markerWidth="10" markerHeight="10" markerUnits="strokeWidth">
+        <path d="M0,0 L0,6 L9,3 z" fill="navy" />
+      </marker>`
+
+    const polyline = svg`
+      <polyline id="${id}" marker-mid="url(#mid)" points="${points}">
+      </polyline>`
+
+    const rect = svg`
+      <rect width="20" height="20" fill="#7373B9"
+        x="${x - 10}"
+        y="${y - 10}">
+      </rect>`
+
+    const text = svg`
+      <text stroke="navy" text-anchor="middle"
+        x="${x}"
+        y="${y + 5}">
+        ${global.skills[props.id].upstream[upstream]}
+      </text>`
+
+    return html`
+      <svg class="line" width="1012" height="752">
+        ${marker}
+        ${polyline}
+        ${rect}
+        ${text}
+      </svg>`
+  })
+
+  return html`${lines}`
+})
+
+//--------------------------------------------------------------------------------
+//---------------------------------------- Helper Functions ----------------------
+//--------------------------------------------------------------------------------
+const createClasses = classes => {
+  return Object.entries(classes).reduce((acc, [ k, v ]) => {
+    if (v)
+      acc.push(k)
+
+    return acc
+  }, []).join(" ")
+}
+
+//----------------------------------------
+const loadURL = () => {
+  if (!location.hash)
+    return
+
+  const data = JSON.parse(LZString.decompressFromEncodedURIComponent(location.hash))
+
+  //Object.entries(data).forEach(([ k, v ]) => state[k] = v)
+
+  /*
+  var i = 0
+
+  for (var skill in global.classes[state.currentClass].skills) {
+    $scope.skillAllocation[global.classes[state.currentClass].skills[skill]] = $scope.saveData.Skills[i]
+    $scope.skillPoints.usedSkillPoints += $scope.saveData.Skills[i] == undefined
+      ? 0
+      : $scope.saveData.Skills[i]
+    i++
+  }
+
+  const retiredSp = $scope.retirement.retirementData[$scope.retirement.selected] || 0
+
+  $scope.skillPoints.totalSkillPoints = parseInt($scope.skillPoints.initialSP) + parseInt($scope.level.selected) + parseInt(retiredSp)
+  //*/
+}
+
+//----------------------------------------
+const saveURL = () => {
+  const keys = [ "currentClass", "currentLevel", "currentRetirement", "skillAllocation" ]
+  const data = {}
+
+  keys.forEach(key => data[key] = state[key])
+
+  const dataURI = LZString.compressToEncodedURIComponent(JSON.stringify(data))
+
+  history.replaceState(null, "", `#${dataURI}`)
+  //location.hash = dataURI
+}
+
+//----------------------------------------
+const handleDrawLines = () => document.getElementsByClassName("svg-grid")[0]?.children?.forEach(child => midMarkers(child.querySelector("polyline")))
+
+//----------------------------------------
+// https://stackoverflow.com/questions/11808860/how-to-place-arrow-head-triangles-on-svg-lines
+const midMarkers = poly => {
+  var svg = poly.ownerSVGElement;
+
+  for (var pts = poly.points, i = 1; i < pts.numberOfItems; ++i) {
+    var p0 = pts.getItem(i - 1),
+        p1 = pts.getItem(i);
+
+    var dx = p1.x - p0.x,
+        dy = p1.y - p0.y;
+
+    var d = Math.sqrt(dx * dx + dy * dy);
+    var numPoints = Math.floor(d / 16);
+
+    dx /= numPoints;
+    dy /= numPoints;
+
+    for (var j = numPoints - 1; j > 0; --j) {
+      var pt = svg.createSVGPoint();
+      pt.x = p0.x + dx * j;
+      pt.y = p0.y + dy * j;
+      pts.insertItemBefore(pt, i);
+    }
+
+    if (numPoints > 0)
+      i += numPoints - 1;
+  }
+}
+
+//--------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------
+init()
